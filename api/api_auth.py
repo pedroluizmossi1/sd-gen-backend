a1e479d0fd3f96d05508bfa24f75f93699e0e2f8
Pedro Luiz mossi <79431501+pedroluizmossi1@users.noreply.github.com>
2023-06-11 22:07:02 -0300

    Add files via upload

---
+++ api/api_auth.py
@@ -0,0 +1,49 @@
+from fastapi import APIRouter, Depends, HTTPException
+from fastapi import Depends, HTTPException, status
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
+import json
+from mongo.mongo_core import login_user
+import mongo.mongo_models as mongo_models
+from redis_core import insert_json, get_json, delete_json
+
+auth_scheme = HTTPBearer()
+
+router_auth = APIRouter(
+    prefix="/auth",
+    tags=["auth"],
+    responses={404: {"description": "Not found"}},
+)
+
+@router_auth.post("/login")
+async def login(user_login: mongo_models.User.Login):
+    if login_user(user_login.login, user_login.password):
+        try:
+            token = insert_json(json.dumps({"login":user_login.login}), 36000) 
+            return {"message": "Login success", "token": token}
+        except:
+            raise HTTPException(status_code=500, detail="Failed to create token") 
+    else:
+        raise HTTPException(status_code=401, detail="Login failed")
+
+@router_auth.post("/logout")
+async def logout(user_logout: mongo_models.User.Logout):
+    try:
+        delete_json(user_logout.token)
+        return {"message": "Logout success"}
+    except:
+        raise HTTPException(status_code=500, detail="Failed to Logout")
+    
+@router_auth.get("/check")
+async def check(token: str):
+    if get_json(token):
+        return True
+    else:
+        raise HTTPException(status_code=401, detail="Token invalid")
+    
+async def authorize_token(credentials: HTTPAuthorizationCredentials = Depends(auth_scheme)):
+    response = get_json(credentials.credentials)
+    if response:
+        return json.loads(response)["login"]
+    else:
+        raise HTTPException(status_code=401, detail="Token invalid")
+
